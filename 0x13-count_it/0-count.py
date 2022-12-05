b5a904d9a24684840c6f7978773c4991289f087a
Lamine Mokhtar Jemli <113144795+laminemokhtarjemli@users.noreply.github.com>
2022-12-05 11:07:26 +0100

    Add files via upload

---
+++ 0x13-count_it/0-count.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" count words
+"""
+from requests import get
+
+
+def count_words(subreddit, word_list=[], after=''):
+
+	hot = "https://www.reddit.com/r/{}/hot.json?after={}".format(subreddit,
+                                                                 after)
+	stuff = hot_req.json()
+	if stuff.get("data") and stuff['data'].get('children'):
+		children = stuff.get("data").get("children")
+		for child in children:
+			word_list.append(child.get("data").get("title"))
+
+		page = stuff.get("data").get("after")
+		if page:
+			return count_words(subreddit, word_list, page)
+
+	if word_list == []:
+		return None
+	return word_list
