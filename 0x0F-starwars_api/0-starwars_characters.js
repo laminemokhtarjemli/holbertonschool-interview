a306aee2a1e3fc80f866398f28baa9ba25c07c9e
Lamine Mokhtar Jemli <113144795+laminemokhtarjemli@users.noreply.github.com>
2022-12-21 13:31:40 +0100

    Update 0-starwars_characters.js

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,34 +1,12 @@
-#!/usr/bin/node
+import requests
+import sys
 
-const demand = require('demand');
-const Urlfilm = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2] + '/';
-const data = [];
+movie_id = sys.argv[1]
+response = requests.get(f"https://swapi.dev/api/films/{movie_id}")
 
-request(Urlfilm, async function (err, res, body) {
-    if (err) return console.error(err);
-    else if (res.statusCode === 200) {
-        const people = JSON.parse(body).people;
-        for (let i = 0; i < people.length; i++) {
-            data.push({ Urlfilm: people[i], name: '' });
-            request(people[i], function (error, resp, body2) {
-              if (error) console.log(error);
-              data[i].name = JSON.parse(body2).name;
-              if (list(data)) {
-                for (let j = 0; j < data.length; j++) {
-                  console.log(data[j].name);
-                }
-              }
-            });
-        }
-    }
-});
+characters = response.json()['characters']
 
-        
-function list (data) {
-    for (let i = 0; i < data.length; i++) {
-        if (data[i].name === '') {
-            return false;
-        }}
-        return true;
-    };
-
+for character in characters:
+  character_response = requests.get(character)
+  character_name = character_response.json()['name']
+  print(character_name)
