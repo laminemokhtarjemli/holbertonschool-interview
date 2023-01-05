5570d46170ccfe3f89a9273a2c617f98a94ff363
Lamine Mokhtar Jemli <113144795+laminemokhtarjemli@users.noreply.github.com>
2023-01-05 20:54:48 +0100

    Update 0-count.py

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -1,25 +1,51 @@
-#!/usr/bin/python3
-""" count words
-"""
-from requests import get
+import requests
 
+def count_words(subreddit, word_list, after=None):
+  # Set the base URL and headers for the request
+  url = f'https://www.reddit.com/r/{subreddit}/hot.json'
+  headers = {'User-Agent': 'MyBot/1.0'}
 
-def count_words(subreddit, word_list=[], after=''):
-	headers = {''}
+  # Set the parameters for the request
+  params = {'limit': 100}
+  if after:
+    params['after'] = after
 
-	hot = "https://www.reddit.com/r/{}/hot.json?after={}".format(subreddit,
-                                                                 after)
-	reqhot = get(hot, headers=headers)
-	stuff = reqhot.json()
-	if stuff.get("data") and stuff['data'].get('children'):
-		children = stuff.get("data").get("children")
-		for child in children:
-			word_list.append(child.get("data").get("title"))
+  # Make the request and get the response
+  response = requests.get(url, headers=headers, params=params)
 
-		page = stuff.get("data").get("after")
-		if page:
-			return count_words(subreddit, word_list, page)
+  # If the response is not successful, return
+  if response.status_code != 200:
+    return
 
-	if word_list == []:
-		return None
-	return word_list
+  # Get the data from the response
+  data = response.json()
+
+  # Get the list of hot articles
+  hot_articles = data['data']['children']
+
+  # Initialize a dictionary to store the word counts
+  word_counts = {}
+
+  # Iterate through the hot articles and count the words
+  for article in hot_articles:
+    title = article['data']['title']
+    for word in word_list:
+      if word.lower() in title.lower():
+        if word.lower() in word_counts:
+          word_counts[word.lower()] += 1
+        else:
+          word_counts[word.lower()] = 1
+
+  # Get the name of the next page
+  after = data['data']['after']
+
+  # If there is a next page, make a recursive call to count_words
+  if after:
+    count_words(subreddit, word_list, after)
+
+  # Sort the word counts in descending order by count, then alphabetically
+  sorted_word_counts = sorted(word_counts.items(), key=lambda x: (-x[1], x[0]))
+
+  # Print the sorted word counts
+  for word, count in sorted_word_counts:
+    print(f'{word}: {count}')
