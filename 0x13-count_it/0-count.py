a765c92633589661749c8b8585bbd71d98e34069
Lamine Mokhtar Jemli <113144795+laminemokhtarjemli@users.noreply.github.com>
2023-01-05 21:18:28 +0100

    fix

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -1,51 +1,34 @@
 import requests
+import re
+import sys
 
-def count_words(subreddit, word_list, after=None):
-  # Set the base URL and headers for the request
-  url = f'https://www.reddit.com/r/{subreddit}/hot.json'
-  headers = {'User-Agent': 'MyBot/1.0'}
+def count_words(subreddit, word_list, after=None, count={}):
+    # Make the request to the Reddit API
+    headers = {'User-Agent': 'MyBot/0.0.1'}
+    params = {'limit': 100}
+    if after:
+        params['after'] = after
+    response = requests.get(f'https://www.reddit.com/r/{subreddit}/hot.json', headers=headers, params=params)
 
-  # Set the parameters for the request
-  params = {'limit': 100}
-  if after:
-    params['after'] = after
+    # Check if the subreddit is valid
+    if response.status_code == 404:
+        return
 
-  # Make the request and get the response
-  response = requests.get(url, headers=headers, params=params)
-
-  # If the response is not successful, return
-  if response.status_code != 200:
-    return
-
-  # Get the data from the response
-  data = response.json()
-
-  # Get the list of hot articles
-  hot_articles = data['data']['children']
-
-  # Initialize a dictionary to store the word counts
-  word_counts = {}
-
-  # Iterate through the hot articles and count the words
-  for article in hot_articles:
-    title = article['data']['title']
-    for word in word_list:
-      if word.lower() in title.lower():
-        if word.lower() in word_counts:
-          word_counts[word.lower()] += 1
-        else:
-          word_counts[word.lower()] = 1
-
-  # Get the name of the next page
-  after = data['data']['after']
-
-  # If there is a next page, make a recursive call to count_words
-  if after:
-    count_words(subreddit, word_list, after)
-
-  # Sort the word counts in descending order by count, then alphabetically
-  sorted_word_counts = sorted(word_counts.items(), key=lambda x: (-x[1], x[0]))
-
-  # Print the sorted word counts
-  for word, count in sorted_word_counts:
-    print(f'{word}: {count}')
+    # Parse the titles of the hot articles
+    data = response.json()
+    titles = [article['data']['title'] for article in data['data']['children']]
+    
+    # Count the occurrences of the words in the word list in the titles
+    for title in titles:
+        for word in word_list:
+            word_lower = word.lower()
+            count[word_lower] = count.get(word_lower, 0) + len(re.findall(r'\b' + word_lower + r'\b', title.lower()))
+    
+    # Recursively call the function with the next page of articles
+    after = data['data']['after']
+    if after:
+        count_words(subreddit, word_list, after, count)
+    else:
+        # Print the results in the required format
+        for word, cnt in sorted(count.items(), key=lambda x: (-x[1], x[0])):
+            print(f'{word}: {cnt}')
