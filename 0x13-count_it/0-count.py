1c70b4b7eb67640ffcaeabf49dfeddfc00151f3a
Lamine Mokhtar Jemli <113144795+laminemokhtarjemli@users.noreply.github.com>
2023-01-08 12:45:11 +0100

    Update 0-count.py

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -1,41 +1,49 @@
-import requests
+#!/usr/bin/python3
 
-USER_AGENT = {
+import requests as r
+
+
+HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
             AppleWebKit/537.36 (KHTML, like Gecko)\
                  Chrome/87.0.4280.88 Safari/537.36'}
 
-def fetch_subreddit_stories(subreddit, after='null'):
-    """Retrieve a list of the top stories on a subreddit.
-    
-    subreddit: string, name of the subreddit to fetch stories from
-    after: string, used to paginate through the stories
-    """
-    url = f"https://www.reddit.com/r/{subreddit}.json?sort=hot&after={after}&limit=100"
-    response = requests.get(url, headers=USER_AGENT, allow_redirects=False)
-    if response.status_code == 200:
-        data = response.json()['data']
-        stories = data['children']
-        next_page = data.get('after')
-        return stories, next_page
-    return [], None
 
-def count_words_in_stories(subreddit, words_to_count):
-    """Count the occurrences of each word in the top stories of a subreddit.
-    
-    subreddit: string, name of the subreddit to fetch stories from
-    words_to_count: list of strings, words to count in the stories
-    """
-    word_counts = {word: 0 for word in words_to_count}
-    stories, next_page = fetch_subreddit_stories(subreddit)
-    while stories:
-        for story in stories:
-            title = story['data']['title'].lower()
-            for word in word_counts:
-                word_counts[word] += title.split().count(word.lower())
-        stories, next_page = fetch_subreddit_stories(subreddit, after=next_page)
-    
-    sorted_counts = sorted(word_counts.items(), key=lambda kv: (-kv[1], kv[0]))
-    for word, count in sorted_counts:
-        if count > 0:
-            print(f"{word}: {count}")
+def recursive_call(reddit, top_list=[], after="null"):
+    URL = "https://www.reddit.com/r/{}.json?sort=hot&after={}&limit=100"\
+          .format(reddit, after)
+
+    try:
+        subscribers = r.get(URL, headers=HEADERS,
+                            allow_redirects=False).json()
+        data = subscribers.get("data")
+        after = subscribers.get("data").get("after")
+        top_list += [story.get("data")['title'] for story in data['children']]
+        if after:
+            recursive_call(reddit, top_list, after)
+        return top_list
+    except Exception:
+        pass
+
+
+def count_words(reddit, word_list, array=None, dic={}):
+
+    if dic == {}:
+        for word in word_list:
+            dic.update({word: 0})
+    if array is None:
+        array = recursive_call(reddit)
+    if array and len(array) > 0:
+        if word_list != []:
+            word_list = list(set(word_list))
+            for item in array:
+                item = item.lower()
+                if word_list[-1].lower() in item.split():
+                    if word_list[-1] in dic.keys():
+                        dic[word_list[-1]] += 1
+            word_list.pop(-1)
+            count_words(reddit, word_list, array, dic)
+        else:
+            for key in sorted(dic.items(), key=lambda kv: (-kv[1], kv[0])):
+                if key[1] != 0:
+                    print("{}: {}".format(key[0], key[1]))
