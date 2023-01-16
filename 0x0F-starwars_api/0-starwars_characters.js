a64595b0330b2cb057ac119b59854d98716f09f9
Lamine Mokhtar Jemli <113144795+laminemokhtarjemli@users.noreply.github.com>
2023-01-16 21:02:36 +0100

    Update 0-starwars_characters.js

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,23 +1,26 @@
+#!/usr/bin/node
 const request = require('request');
-const MovieID = process.argv[2];
-const url = `https://swapi-api.hbtn.io/api/films/${MovieID}`;
 
-async function getCharacterNames() {
+async function getCharacterNames(urls, index = 0) {
   try {
-    const movieData = await request({uri: url, json: true});
-    if(movieData.detail) {
-      console.log(`Error: ${movieData.detail}`);
-      return;
-    }
-    const characters = movieData.characters;
-    const characterPromises = characters.map(url => request({uri: url, json: true}));
-    const characterData = await Promise.all(characterPromises);
-    characterData.forEach(charData => {
-      console.log(charData.name);
-    });
+    if (index >= urls.length) return;
+    const characterData = await request({uri: urls[index], json: true});
+    console.log(characterData.name);
+    await getCharacterNames(urls, index + 1);
   } catch (err) {
     console.error(err);
   }
 }
 
-getCharacterNames();
+(async () => {
+  try {
+    const movieData = await request({uri: `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`, json: true});
+    if(movieData.detail) {
+      console.log(`Error: ${movieData.detail}`);
+      return;
+    }
+    await getCharacterNames(movieData.characters);
+  } catch (err) {
+    console.error(err);
+  }
+})();
