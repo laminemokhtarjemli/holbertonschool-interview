32855debf508f8193e11f3d34f3d39de22369742
Lamine Mokhtar Jemli <113144795+laminemokhtarjemli@users.noreply.github.com>
2023-01-16 20:55:26 +0100

    Update 0-starwars_characters.js

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,12 +1,23 @@
-import requests
-import sys
+const request = require('request-promise-native');
+const MovieID = process.argv[1];
+const url = `https://swapi-api.hbtn.io/api/films/${MovieID}`;
 
-movie_id = sys.argv[1]
-response = requests.get("https://swapi-api.hbtn.io/api/films/{movie_id}")
+async function getCharacterNames() {
+  try {
+    const movieData = await request({uri: url, json: true});
+    if(movieData.detail) {
+      console.log(`Error: ${movieData.detail}`);
+      return;
+    }
+    const characters = movieData.characters;
+    const characterPromises = characters.map(url => request({uri: url, json: true}));
+    const characterData = await Promise.all(characterPromises);
+    characterData.forEach(charData => {
+      console.log(charData.name);
+    });
+  } catch (err) {
+    console.error(err);
+  }
+}
 
-characters = response.json()['characters']
-
-for character in characters:
-  character_response = requests.get(character)
-  character_name = character_response.json()['name']
-  print(character_name)
+getCharacterNames();
