fb3cf4d7d0e4275fb25228c557f5bac94cc68c63
Lamine Mokhtar Jemli <113144795+laminemokhtarjemli@users.noreply.github.com>
2023-01-16 21:10:39 +0100

    Update 0-starwars_characters.js

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,46 +1,39 @@
 #!/usr/bin/node
 const request = require('request');
 
-function getCharacterNames(urls, index = 0) {
-  request(urls[index], (err, res, body) => {
-    if (!err) {
-      const character = JSON.parse(body);
-      console.log(character.name);
-      if (index + 1 < urls.length) getCharacterNames(urls, index + 1);
-    }
+async function getCharacterNames(urls, index = 0) {
+  if (index >= urls.length) return;
+  return new Promise((resolve, reject) => {
+    request(urls[index], (err, res, body) => {
+      if (!err) {
+        const character = JSON.parse(body);
+        console.log(character.name);
+        resolve(getCharacterNames(urls, index + 1));
+      } else {
+        reject(err);
+      }
+    });
   });
 }
 
-request(`https://swapi-api.hbtn.io/api/films/${process.argv[2]}`, (err, res, body) => {
-  if (!err) {
-    const movie = JSON.parse(body);
-    if(movie.detail) {
-      console.log(`Error: ${movie.detail}`);
-      return;
-    }
-    getCharacterNames(movie.characters);
+(async () => {
+  try {
+    const movieData = await new Promise((resolve, reject) => {
+      request(`https://swapi-api.hbtn.io/api/films/${process.argv[2]}`, (err, res, body) => {
+        if (!err) {
+          const movie = JSON.parse(body);
+          if(movie.detail) {
+            console.log(`Error: ${movie.detail}`);
+            return;
+          }
+          resolve(movie);
+        } else {
+          reject(err);
+        }
+      });
+    });
+    await getCharacterNames(movieData.characters);
+  } catch (err) {
+    console.error(err);
   }
-});
-#!/usr/bin/node
-const request = require('request');
-
-function getCharacterNames(urls, index = 0) {
-  request(urls[index], (err, res, body) => {
-    if (!err) {
-      const character = JSON.parse(body);
-      console.log(character.name);
-      if (index + 1 < urls.length) getCharacterNames(urls, index + 1);
-    }
-  });
-}
-
-request(`https://swapi-api.hbtn.io/api/films/${process.argv[2]}`, (err, res, body) => {
-  if (!err) {
-    const movie = JSON.parse(body);
-    if(movie.detail) {
-      console.log(`Error: ${movie.detail}`);
-      return;
-    }
-    getCharacterNames(movie.characters);
-  }
-});
+})();
