8dbce932ed8bb494b185a5e358a4a8d7dc441262
Lamine Mokhtar Jemli <113144795+laminemokhtarjemli@users.noreply.github.com>
2023-02-13 19:45:16 +0100

    Update 0-starwars_characters.js

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,39 +1,23 @@
 #!/usr/bin/node
+
 const request = require('request');
 
-async function getCharacterNames(urls, index = 0) {
-  if (index >= urls.length) return;
-  return new Promise((resolve, reject) => {
-    request(urls[index], (err, res, body) => {
-      if (!err) {
-        const character = JSON.parse(body);
-        console.log(character.name);
-        resolve(getCharacterNames(urls, index + 1));
-      } else {
-        reject(err);
-      }
-    });
-  });
-}
+const filmNum = process.argv[2] + '/';
+const filmURL = 'https://swapi-api.hbtn.io/api/films/';
 
-(async () => {
-  try {
-    const movieData = await new Promise((resolve, reject) => {
-      request(`https://swapi-api.hbtn.io/api/films/${process.argv[2]}`, (err, res, body) => {
-        if (!err) {
-          const movie = JSON.parse(body);
-          if(movie.detail) {
-            console.log(`Error: ${movie.detail}`);
-            return;
-          }
-          resolve(movie);
-        } else {
-          reject(err);
-        }
+request(filmURL + filmNum, async function (err, res, body) {
+  if (err) return console.error(err);
+
+  const charURLList = JSON.parse(body).characters;
+
+  for (const charURL of charURLList) {
+    await new Promise(function (resolve, reject) {
+      request(charURL, function (err, res, body) {
+        if (err) return console.error(err);
+
+        console.log(JSON.parse(body).name);
+        resolve();
       });
     });
-    await getCharacterNames(movieData.characters);
-  } catch (err) {
-    console.error(err);
   }
-})();
+});
