9fa35bcd2ecd1c396688cbdefe74c985ec57b50a
Lamine Mokhtar Jemli <113144795+laminemokhtarjemli@users.noreply.github.com>
2023-02-14 18:55:06 +0100

    Update 0-nqueens.py

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -2,56 +2,88 @@
 """script that uses backtracking to solve the N-Queens problem. 
     The N-Queens problem is the task of placing N chess queens on an
     NxN chessboard"""
-import sys
+from sys import argv
 
 
-def n_queens(x, z, board):
-        """
-        Function: task of placing N chess queens
-                  on an NxN chessboard so that no
-                  two queens threaten each other.
+def validate() -> None:
+ 
+    if (len(argv) != 2):
+        print('Usage: nqueens N') or exit(1)
+    try:
+        num = int(argv[1])
+    except ValueError:
+        print('N must be a number') or exit(1)
+    if (num < 4):
+        print('N must be at least 4') or exit(1)
+    nqueens(0, num)
 
-        Parameters: The x argument represents the size of the chessboard
-                    and the number of queens to be placed
-        
-        Return: All possible solutions to
-            placement, in list of lists form
-        """
-        for i in range(x):
-            hold = 0
-            for q in board:
-                if i == q[1]:
-                    hold = 1
-                    break
-                if z - i == q[0] - q[1]:
-                    hold = 1
-                    break
-                if i - q[1] == q[0] - z:
-                    hold = 1
-                    break
-            if hold == 0:
-                board.append([z, i])
-                if z != x - 1:
-                    n_queens(x, z + 1, board)
-                else:
-                    print(board)
-                del board[-1]
+       def nqueens(j: int, num: int) -> None:
+    """N Queens Algorithm"""
+    table = [num * [0] for i in range(num)]
+    col = []
+    diag_right = []
+    diag_left = []
+    dd = []
+    create_table(j, num, 0, table, diag_right, diag_left, col, dd)
+    ef create_table(j: int, num: int, bol: int, table: int, diag_right: list,
+                 diag_left: list, col: list, dd: list) -> None:
+    if bol == 0:
+        i = 0
+    else:
+        i = num - 1
+    while (i < num):
+        flag = 0
+        while (j < num):
+            if check(num, i, j, diag_right, diag_left, col):
+                table[i][j] = 1
+                diag_right.append(i - j)
+                diag_left.append((num - 1) - (i + j))
+                col.append(j)
+                dd.append([i, j])
+                flag = 1
+                break
+            j += 1
+        if flag == 0:
+            try:
+                idx = table[i - 1].index(1)
+            except ValueError:
+                return
+            table[i - 1][idx] = 0
+            diag_right.pop()
+            diag_left.pop()
+            col.pop()
+            dd.pop()
+            i -= 1
+            j = idx + 1
+            continue
+        j = 0
+        i += 1
+    if i == num and flag == 0:
+        return
+    print(dd)
+    idx = table[i - 1].index(1)
+    table[i - 1][idx] = 0
+    diag_right.pop()
+    diag_left.pop()
+    col.pop()
+    dd.pop()
+    create_table(idx + 1, num, 1, table, diag_right, diag_left, col, dd)
 
 
-def main():
-        if len(sys.argv) != 2:
-            print("Usage: nqueens N")
-            sys.exit(1)
-        try:
-            n = int(sys.argv[1])
-        except Exception:
-            print('N must be a number')
-            sys.exit(1)
-        if n < 4:
-            print("N must be at least 4")
-            sys.exit(1)
+def check(num: int, i: int, j: int,
+          right: list, left: list, col: list) -> bool:
+    # Diagonal right
+    if (i - j) in right:
+        return (False)
+    # Diagonal left
+    if ((num - 1) - (j + i)) in left:
+        return (False)
+    # Column
+    if j in col:
+        return (False)
+    return (True)
 
-        n_queens(n, 0, [])
 
 if __name__ == '__main__':
-        main()
+    validate()
+    
