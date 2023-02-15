aac7032be1708531b35f8024be0361dbb84169c5
Lamine Mokhtar Jemli <113144795+laminemokhtarjemli@users.noreply.github.com>
2023-02-15 13:43:22 +0100

    Update 0-starwars_characters.js

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -4,14 +4,16 @@ const request = require('request');
 
 const movieId = process.argv[2];
 const url = `https://swapi.dev/api/films/${movieId}/`;
+
 request(url, async function (error, response, body) {
   if (error) {
     console.log(error);
   } else {
     const characters = JSON.parse(body).characters;
-    for (const character of characters) {
+
+    for (const characterUrl of characters) {
       const res = await new Promise((resolve, reject) => {
-        request(character, (error, res, html) => {
+        request(characterUrl, (error, res, html) => {
           if (error) {
             reject(error);
           } else {
