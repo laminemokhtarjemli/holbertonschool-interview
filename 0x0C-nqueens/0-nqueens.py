eba409ecf306b9f96a340ee6a733a44907db0043
Lamine Mokhtar Jemli <113144795+laminemokhtarjemli@users.noreply.github.com>
2023-02-20 14:24:20 +0100

    Update 0-nqueens.py

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
-"""Solving N Queens Problem"""
+"""Solving N Queens with Backtracing"""
 import sys
 
 
 def nqueens(n, y, board):
     """
-    Method: nqueens - place queens
-            on an nXn board so that
+    Method: nqueens - place n queens
+            on an n by n board so that
             no queens are attacking any
             others.
     Parameters: n is an int that sets
                 board size and # of queens
-    Return: possible solutions to
+    Return: All possible solutions to
             placement, in list of lists form
     """
     for x in range(n):
