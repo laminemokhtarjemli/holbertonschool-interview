c08789344caa850bdf4eb3d68608c0d37b82b7bf
Lamine Mokhtar Jemli <113144795+laminemokhtarjemli@users.noreply.github.com>
2023-02-21 10:43:12 +0100

    Update 0-nqueens.py

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -1,69 +1,41 @@
 #!/usr/bin/python3
 """ n queens """
-import sys
+rom sys import argv, exit
 
-if len(sys.argv) != 2:
-    print("Usage: nqueens N")
-    exit(1)
-try:
-    int(sys.argv[1])
-except Exception:
-    print("N must be a number")
-    exit(1)
-if int(sys.argv[1]) < 4:
-    print("N must be at least 4")
-    exit(1)
-if not isinstance(int(sys.argv[1]), int):
-    print("N must be a number")
-    exit(1)
+if __name__ == "__main__":
+    if len(argv) != 2:
+        print('Usage: nqueens N')
+        exit(1)
+    try:
+        n = int(argv[1])
+    except BaseException:
+        print('N must be a number')
+        exit(1)
+    if n < 4:
+        print('N must be at least 4')
+        exit(1)
 
-n = int(sys.argv[1])
+    solution = []
 
+    def solve_queens(row, n, solution):
+        if (row == n):
+            print(solution)
+        else:
+            for col in range(n):
+                placement = [row, col]
+                if valid_placement(solution, placement):
+                    solution.append(placement)
+                    solve_queens(row + 1, n, solution)
+                    solution.remove(placement)
 
-def is_valid_state(state, n):
-    return len(state) == n
+    def valid_placement(solution, placement):
+        for queen in solution:
+            if queen[1] == placement[1]:
+                return False
+            if (queen[0] + queen[1]) == (placement[0] + placement[1]):
+                return False
+            if (queen[0] - queen[1]) == (placement[0] - placement[1]):
+                return False
+        return True
 
-
-def get_candidates(state, n):
-    if not state:
-        return range(n)
-
-    position = len(state)
-    candidates = set(range(n))
-    for row, col in enumerate(state):
-        candidates.discard(col)
-        dist = position - row
-        candidates.discard(col + dist)
-        candidates.discard(col - dist)
-    return candidates
-
-
-def search(state, solutions, n):
-    if is_valid_state(state, n):
-        state_string = state_to_string(state)
-        solutions.append(state_string)
-        return
-
-    for candidate in get_candidates(state, n):
-        state.append(candidate)
-        search(state, solutions, n)
-        state.pop()
-
-
-def solveNQueens(n):
-    solutions = []
-    state = []
-    search(state, solutions, n)
-    return solutions
-
-
-def state_to_string(state):
-    res = []
-
-    for x, y in enumerate(state):
-        res.append([x, y])
-    return res
-
-
-for solution in solveNQueens(n):
-    print(solution)
+    solve_queens(0, n, solution)
